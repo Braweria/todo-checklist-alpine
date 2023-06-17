d828404c57a16c16f6decafd76e23b55880c3af3
Wiktoria Mielcarek <62669899+Braweria@users.noreply.github.com>
2023-06-17 15:42:58 +0200

    feat: view trash bin and restore/delete trashed items

--- main.js
+++ main.js
@@ -3,14 +3,16 @@ import Alpine from "alpinejs";
 window.Alpine = Alpine;
 
 const STORAGE_ITEMS_KEY = "checklist-items";
+const STORAGE_TRASH_KEY = "checklist-items-trash";
 
 function createItem(name) {
   return {
     name,
     completed: false,
-    id: Date.now() + Math.random(),
+    id: Math.round(Date.now() + Math.random()),
   };
 }
+
 Alpine.data("checklist", () => ({
   item: "",
   items: [],
@@ -23,9 +25,20 @@ Alpine.data("checklist", () => ({
     if (storedItems) {
       this.items = storedItems;
     }
+    const storedTrashedItems = JSON.parse(
+      localStorage.getItem(STORAGE_TRASH_KEY)
+    );
+    if (storedTrashedItems) {
+      this.trash = storedTrashedItems;
+    }
 
     this.$watch("items", (value) => {
       localStorage.setItem(STORAGE_ITEMS_KEY, JSON.stringify(value));
+    });
+
+    this.$watch("trash", (value) => {
+      console.log(value);
+      localStorage.setItem(STORAGE_TRASH_KEY, JSON.stringify(value));
     });
   },
 
@@ -38,12 +51,23 @@ Alpine.data("checklist", () => ({
 
   remove(id) {
     const index = this.items.findIndex((item) => item.id === id);
-    this.trash.push(this.items.splice(index, 1));
+    this.trash.push(this.items[index]);
+    this.items.splice(index, 1);
   },
 
   toggleTrash() {
     this.showTrash = !this.showTrash;
-  }
+  },
+
+  restore(id) {
+    const index = this.trash.findIndex((item) => item.id === id);
+    this.items.push(this.trash[index]);
+    this.trash.splice(index, 1);
+  },
+
+  deleteForever(id) {
+    this.trash = this.trash.filter((item) => item.id !== id);
+  },
 }));
 
 Alpine.start();
